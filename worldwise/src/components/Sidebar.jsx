647ce98e6cb031121cb616914b53fc7755ea30cf
Divyesh Parikh <dcparikh360@gmail.com>
2025-04-30 20:24:18 +0530

    Added Router

---
+++ worldwise/src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { Outlet } from 'react-router-dom';
+import styles from './Sidebar.module.css';
+import Logo from './Logo';
+import AppNav from './AppNav'
+
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+
+      <Logo/>
+      <AppNav/>
+
+      <Outlet/>
+      
+      <footer className={styles.footer}>
+      <p className={styles.copyright}>
+        &copy; Copyright {new Date().getFullYear()}
+      </p>
+
+      </footer>
+
+    </div>
+  )
+}
+
+export default Sidebar
